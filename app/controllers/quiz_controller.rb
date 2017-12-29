2143f74498fb96985d0efd64035ca1f8bf939869
nikitasakov <nikitasakov@mail.ru>
2017-12-29 22:50:52 +0300

    rtgrt

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,35 +1,43 @@
 class QuizController < ApplicationController
-	skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-	def index
+  def index
+    s_file = File.read('database.json')
+    str = JSON.parse(s_file)
+    @per = str[0][0]
   end
 
   def task
-	info_string = File.read('database.json')
-    	info = JSON.parse(info_string)
-  	string = params["question"]
-	level = params["level"].to_i
-	id = params["id"]
-	#answer = Pushkin.up(level, string, info)
-	case level
-			when 1
-				info.size.times do |i|
-					if info[i][1].include?(string)
-						answer = info[i][0]
-						break
-					end
-				end
+    info_string = File.read('database.json')
+    info = JSON.parse(info_string)
+    answer = ""
+    string = params["question"]
+    level = params["level"].to_i
+    id = params["id"]
+    case level
+      when 1
+        info.size.times do |i|
+		if info[i][1].include?(string)
+			answer = info[i][0]
+			break
+		end
 	end
+    end
+    if answer
+      uri_app = URI('http://pushkin.rubyroidlabs.com/quiz')
 
-	
-	uri = URI('http://pushkin.rubyroidlabs.com/quiz')
-	parameters = {
+      parameters = {
         answer: answer,
-        token: "62a577a09ddc094e97031892510c16fd",
+        token: '62a577a09ddc094e97031892510c16fd',
         task_id: id
-    }
-    res = Net::HTTP.post_form(uri, parameters)
+      }
+      Net::HTTP.post_form(uri_app, parameters)
+      #render json: 'ok'
+      #puts res.body
+    end
+    file = File.open('in_data.json', 'w') do |f|
+      f.write(params)
+    end
   end
 end
-
 
